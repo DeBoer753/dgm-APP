1c95c27900c4b5b34f33f74ab9acb506b5be7514
deboer753 <myles.deboer@gmail.com>
2025-02-19 17:22:53 -0800

    testign to see if email form works after insertign formspree id and global css html taken away to fix banner fade

--- src/app/pages/contact.tsx
+++ src/app/pages/contact.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
     const formData = new FormData(e.currentTarget);
 
     try {
-      const response = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const response = await fetch("https://formspree.io/f/mrbervaz", {
         method: "POST",
         body: formData,
         headers: {
